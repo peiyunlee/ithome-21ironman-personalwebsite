9d09ae04297ceab0bfcf1d3f083c1f9db8ce467f
PEI-YUN LEE <may1092200258@gmail.com>
2021-08-19 17:15:47 +0800

    [refresh] - variable name

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-let target = document.getElementById("header-content");
-let button = document.getElementById("hamburger");
+let tar = document.getElementById("header-content");
+let ham = document.getElementById("hamburger");
 
-button.addEventListener("click", function (event) {
-  target.classList.toggle("header-content_active");
+ham.addEventListener("click", function (event) {
+  tar.classList.toggle("header-content_active");
 });
